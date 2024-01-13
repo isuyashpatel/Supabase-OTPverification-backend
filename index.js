77b06b94ae046c5b25aeb1cdc11f22e353420fed
isuyashpatel <suyashpatelmrj@gmail.com>
2024-01-13 22:26:18 +0530

    /- cause cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: [ 'http://localhost:3000','https://pixel-ui-beta.vercel.app/']
+  origin: [ 'http://localhost:3000','https://pixel-ui-beta.vercel.app']
 }));
 
 
